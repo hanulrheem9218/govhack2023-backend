5d6355d2d5e85bcb0050fd61c4a018a68d10d3d4
dgw7626 <84116773+dgw7626@users.noreply.github.com>
2023-08-19 16:38:11 +1200

    Updated firebase settings.

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -2,8 +2,10 @@ const admin = require("firebase-admin");
 
 require('dotenv').config();
 
+const accountConfig =JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+
 admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON))
+    credential: admin.credential.cert(accountConfig)
 });
 
 module.exports = admin;
