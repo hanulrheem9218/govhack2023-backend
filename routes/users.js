349e30b730da705a2b75247fecd8acc8e31cceb0
Jun Jung <junjung@canva.com>
2023-08-19 19:48:57 +1200

    [Update] Removing unnecessary `user` model fields.
    - Update `user` APIs.

--- routes/users.js
+++ routes/users.js
@@ -9,30 +9,49 @@ const User = require('../models/user');
 // @route GET api/users/
 // @description Create new user
 // @access Public
-router.post("/", (req, res) => {
-    User.create(req.body)
-        .then(user => res.status(201).json({ message: "User added successfully", user: user }))
-        .populate({
-            path: "achievements_history",
-            polulate: {
-                path: "achievement"
+router.get("/", (req, res) => {
+    if(req.authorizationContext == null) {
+        res.status(403).json({ message: "User unauthorized." });
+    } else {
+        User.findOne({ "user_id": req.authorizationContext.user_id })
+        .then((user) => {
+            if(user == null) {
+                User.create({ user_id: req.authorizationContext.user_id, name: "Green Kiwi User", points: 0, profile_image: "", achievements_history: [] })
+                .then(user => res.json({ message: "Successfully retrieved user.", user: user }))
+                .catch(error => {
+                    console.error(error);
+                    res.status(500).json({ message: "Failed to retrieve user." })});
+            } else {
+                res.json({ message: "Successfully retrieved user.", user: user });
             }
         })
-        .catch(err =>{
-            console.log(err);
-            res.status(400).json({ message: "Unable to create a user." })});
+        .catch(error => {
+            console.error(error);
+            console.warn("Failed to retrieve user.. trying to create with given User ID.");
+        }); 
+    }
 });
 
 // @route GET api/user/:id
 // @description Update user
 // @access Public
-router.put('/:id', (req, res) => {
-    User.findByIdAndUpdate(req.params.id)
-        .then(user => res.json({ message: "User updated successfully.", user: user }))
-        .catch(err =>{ 
-            console.log(err);
-            console.log(req.json);
-            res.status(400).json({ message: "Unable to update user." })});
+router.put('/', (req, res) => {
+    if(req.authorizationContext == null) {
+        res.status(403).json({ message: "User unauthorized." });
+    } else {
+        User.findOneAndUpdate({ "user_id": req.authorizationContext.user_id }, req.body )
+        .then(_ => res.json({ message: "Successfully updated user." }))
+        .catch(error => {
+            console.error(error);
+            console.warn("Failed to retrieve user. trying to create with given User ID.");
+
+            User.create({ ...req.body, user_id: req.authorizationContext.user_id })
+            .then(_ => res.json({ message: "Successfully updated user." }))
+            .catch(error => {
+                console.error(error);
+                res.status(500).json({ message: "Failed to retrieve user." })});
+            });
+    }
 });
 
 module.exports = router;
