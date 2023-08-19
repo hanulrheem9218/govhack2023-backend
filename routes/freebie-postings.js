e5c79f3a5e0ba6475e7c5ebc2311b0ba971c6c4a
Jun Jung <junjung@canva.com>
2023-08-19 21:02:51 +1200

    [Update] Updating some coding styles and removing unused codes.

--- routes/freebie-postings.js
+++ routes/freebie-postings.js
@@ -1,11 +1,8 @@
-// routes/api/books.js
-
 const express = require('express');
 const router = express.Router();
 
 // Load Post model
 const FreebiePosting = require('../models/freebie-posting');
-const freebiePosting = require('../models/freebie-posting');
 
 // @route GET api/freebie-posting/all
 // @description Get all posts
@@ -16,21 +13,14 @@ router.get("/all", (_, res) => {
         .catch(_ => res.status(404).json({ message: "No freebie postings found" }));
 });
 
-//location: String
-//title: String
-//description: String
-//image_urls: List[String]
-//is_listing_closed: Bool
-
-
 // @route GET api/freebie-postings/
 // @description Create new post
 // @access Public
 router.post("/", (req, res) => {
     // you can setup the default values here.
-    FreebiePosting.create({...req.body,image_urls: new Array(), is_listing_closed : false})
+    FreebiePosting.create({ ...req.body, image_urls: new Array(), is_listing_closed : false })
         .then(freebiePosting => res.status(201).json({ message: "Freebie posting added successfully", freebiePosting: freebiePosting }))
-        .catch(err =>{
+        .catch(err => {
             console.log(err);
             res.status(400).json({ message: "Unable to create a freebie posting." })});
 });
@@ -41,7 +31,7 @@ router.post("/", (req, res) => {
 router.get("/:id", (req, res) => {
     FreebiePosting.findById(req.params.id)
     .then(freebiePosting => res.json(freebiePosting))
-    .catch(err =>{ 
+    .catch(err => { 
         console.log(err);
         res.status(404).json({ message: "No freebie posting with given ID." })});
 });
@@ -52,7 +42,7 @@ router.get("/:id", (req, res) => {
 router.put('/:id', (req, res) => {
     FreebiePosting.findByIdAndUpdate(req.params.id, req.body)
         .then(freebiePosting => res.json({ message: "Freebie posting updated successfully.", freebiePosting: freebiePosting }))
-        .catch(err =>{ 
+        .catch(err => { 
             console.log(err);
             res.status(400).json({ message: "Unable to update freebie posting." })});
 });
@@ -62,7 +52,7 @@ router.put('/:id', (req, res) => {
 // @access Public
 router.delete('/:id', (req, res) => {
     FreebiePosting.findByIdAndRemove(req.params.id)
-        .then(freebiePosting => res.json({ message: "Successfully deleted freebiePosting." }))
+        .then(_ => res.json({ message: "Successfully deleted freebiePosting." }))
         .catch(err => {
             console.log(err);
             res.status(404).json({ message: "No freebie posting with given ID." })});
